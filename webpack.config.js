1844848824c61c408691a73ee33b036c266b7f96
pugovka <ms.n.yunak@gmail.com>
2016-11-08 21:10:29 +1000

    Init project with webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
