360962740598452cb1ed9b19a6f0fc5274c9a7da
pugovka <ms.n.yunak@gmail.com>
2016-11-10 15:17:26 +1000

    Add script to get records from bd

---
+++ php/get-records-list.php
@@ -0,0 +1,27 @@
+<?php
+header('Access-Control-Allow-Origin: http://localhost:8080');
+include('connect.php');
+
+$recordsListQuery = '
+  SELECT
+    phonebook.id,
+    phonebook.last_name,
+    phonebook.first_name,
+    phonebook.second_name,
+    IFNULL(cities.city_name, "") city_name,
+    IFNULL(streets.street_name, "") street_name,
+    phonebook.birth_date,
+    phonebook.phone_number
+  FROM phonebook
+  LEFT JOIN cities ON phonebook.city_id=cities.id
+  LEFT JOIN streets ON phonebook.street_id=streets.id
+  ORDER BY last_name ASC;';
+
+if ($recordsList = $connection->query($recordsListQuery)) {
+  $arrRecords = array();
+  while ($record = $recordsList->fetch_assoc()) {
+    $arrRecords[] = $record;
+  }
+}
+
+echo json_encode($arrRecords);
