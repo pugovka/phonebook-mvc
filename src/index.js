303478c730c3ccfc77ba40521935680b62961e0e
pugovka <ms.n.yunak@gmail.com>
2016-11-11 18:42:37 +1000

    Init phonebook

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 'use strict';
+import PhonebookModel from './model';
+import PhonebookView from './view';
+import PhonebookController from './controller';
+
+const model = new PhonebookModel();
+const view = new PhonebookView();
+const controller = new PhonebookController(model, view);
