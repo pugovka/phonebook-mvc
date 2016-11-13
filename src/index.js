0ffd737826fa81227be29c6e22fd5d429e534875
pugovka <ms.n.yunak@gmail.com>
2016-11-13 16:12:48 +1000

    Add styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import PhonebookModel from './model';
 import PhonebookView from './view';
 import PhonebookController from './controller';
+import style from './styles.scss';
 
 const model = new PhonebookModel();
 const view = new PhonebookView();
