79110d8a92716a5de5fc78f1d9e1559b4d22cc11
pugovka <ms.n.yunak@gmail.com>
2016-11-13 16:39:56 +1000

    Rerender records after phonebook changes

--- src/controller.js
+++ src/controller.js
@@ -27,37 +27,43 @@ export default class PhonebookController {
     this._view.addRecordButtonClicked.attach((sender, formData) => {
       // Add record to db using model
       this.addRecord(formData);
-      // Update records list
-      this.renderRecords();
     });
 
     // Listen to update record button click from view
     this._view.updateRecordButtonClicked.attach((sender, data) => {
       // Edit record in db using model
       this.editRecord(data.recordId, data.recordData);
-      // Update records list
-      this.renderRecords();
     });
 
     // Listen to delete record button click from view
     this._view.deleteRecordButtonClicked.attach((sender, recordId) => {
       // Delete record from db using model
       this.deleteRecord(recordId);
-      // Update records list
-      this.renderRecords();
     });
   }
 
   addRecord(formData) {
-    this._model.addRecord(formData);
+    this._model.addRecord(formData)
+      .then(() => {
+        // Update records list
+        this.renderRecords();
+      });
   }
 
   editRecord(recordId, formData) {
-    this._model.editRecord(recordId, formData);
+    this._model.editRecord(recordId, formData)
+      .then(() => {
+        // Update records list
+        this.renderRecords();
+      });
   }
 
   deleteRecord(recordId) {
-    this._model.deleteRecord(recordId);
+    this._model.deleteRecord(recordId)
+    .then(() => {
+      // Update records list
+      this.renderRecords();
+    });
   }
 
   renderRecords() {
