f6314d1f51dd775d7c5b690119bd227f439aed76
pugovka <ms.n.yunak@gmail.com>
2016-11-15 10:12:40 +1000

    Change mock data

--- sql/insert-streets.sql
+++ sql/insert-streets.sql
@@ -1,8 +1,11 @@
 INSERT INTO `streets` (`id`, `street_name`) VALUES
+  (NULL, 'Svetlanskaya Street'),
+  (NULL, 'Semenovskaya Street'),
   (NULL, 'Arbat Street'),
   (NULL, 'Druzhby Street'),
   (NULL, 'Mira Avenue'),
   (NULL, 'Admiralty Embankment'),
   (NULL, 'Nevsky Prospekt‎'),
-  (NULL, 'Svetlanskaya Street'),
-  (NULL, 'Semenovskaya Street');
+  (NULL, 'Miasskaya'),
+  (NULL, 'Pirogova'),
+  (NULL, 'Blyukhera');
